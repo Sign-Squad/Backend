0059c45c54bf8535bd3ae585210ffea82de438b8
Juan Diego Huaman Monzón <u202123965@upc.edu.pe>
2024-09-25 16:46:27 -0500

    refactor(utils): remove unused import

--- src/main/java/pe/upc/singlingo_backend/section/application/services/OptionQueryServiceImpl.java
+++ src/main/java/pe/upc/singlingo_backend/section/application/services/OptionQueryServiceImpl.java
@@ -1,17 +1,11 @@
 package pe.upc.singlingo_backend.section.application.services;
 
 import org.springframework.stereotype.Service;
-import pe.upc.singlingo_backend.section.domain.model.aggregates.aggregates.Level;
 import pe.upc.singlingo_backend.section.domain.model.aggregates.aggregates.Option;
-import pe.upc.singlingo_backend.section.domain.model.aggregates.queries.GetLevelByIDQuery;
-import pe.upc.singlingo_backend.section.domain.model.aggregates.queries.GetLevelsBySectionIDQuery;
 import pe.upc.singlingo_backend.section.domain.model.aggregates.queries.GetOptionByIDQuery;
-import pe.upc.singlingo_backend.section.domain.services.LevelQueryService;
 import pe.upc.singlingo_backend.section.domain.services.OptionQueryService;
-import pe.upc.singlingo_backend.section.infraestructure.persistence.jpa.LevelRepository;
 import pe.upc.singlingo_backend.section.infraestructure.persistence.jpa.OptionRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
